496317839362ec12d9ecd7da8ece340405165b56
Nabila Saskia <2511500033@mahasiswa.atmaluhur.ac.id>
2025-12-11 11:40:55 +0700

    update index.php dengan menambahkan flash message dari session

--- pertemuan-10/README.md
+++ pertemuan-10/README.md
@@ -3,4 +3,5 @@ Hai aku Nabila
 hari ini tanggal 11 Desember 2025
 <ol>
 <li>update fungsi php</li>
+<li>update index.php dengan menambahkan flash message dari session</li>
 </ol>
--- pertemuan-10/index.php
+++ pertemuan-10/index.php
@@ -109,8 +109,29 @@ require_once __DIR__ . '/fungsi.php';
       <?= tampilkanBiodata($fieldConfig, $biodata) ?>
     </section>
 
+    <?php
+    $flash_sukses = $_SESSION['flash_sukses'] ?? ''; #jika query sukses
+    $flash_error  = $_SESSION['flash_error'] ?? ''; #jika query error
+    $old          = $_SESSION['old'] ?? []; #untuk nilai lama form
+
+    unset($_SESSION['flash_sukses'], $SESSION['flash_error'], $_SESSION['old']); #bersihkan 3 session ini
+    ?>
+
     <section id="contact">
       <h2>Kontak Kami</h2>
+
+      <?php if (!empty($flash_sukses)): ?>
+        <div style="padding:10px; margin-bottom:10px; background:#d4edda; color:#3155724; border-radius:6px;">
+          <?= $flash_sukses; ?>
+      </div>
+      <?php endif; ?>
+
+      <?php if (!empty($flash_error)): ?>
+        <div style="padding:10px; margin-bottom:10px; background:#f8d7daa; color:#721c24; border-radius:6px;">
+          <?= $flash_error; ?>
+      </div>
+      <?php endif; ?>
+
       <form action="proses.php" method="POST">
 
         <label for="txtNama"><span>Nama:</span>
